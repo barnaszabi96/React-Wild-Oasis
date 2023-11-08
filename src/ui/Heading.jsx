d2ae0746677412ac0320d439d4f6198586081f95
Barna Szabolcs <barnaszabi96@gmail.com>
2023-11-08 11:43:59 +0100

    authentication added

--- src/ui/Heading.jsx
+++ src/ui/Heading.jsx
@@ -12,17 +12,27 @@ const Heading = styled.h1`
       font-size: 3rem;
       font-weight: 600px;
     `}
+
   ${(props) =>
     props.as === "h2" &&
     css`
       font-size: 2rem;
       font-weight: 600px;
     `}
+
   ${(props) =>
     props.as === "h3" &&
     css`
       font-size: 2rem;
       font-weight: 500px;
+    `}
+
+  ${(props) =>
+    props.as === "h4" &&
+    css`
+      font-size: 3rem;
+      font-weight: 600px;
+      text-align: center;
     `}
   line-height: 1.4
 `;
