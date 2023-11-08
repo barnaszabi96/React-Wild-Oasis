b7951fb7131c31b4510009e15fefc10eb52c0116
Barna Szabolcs <barnaszabi96@gmail.com>
2023-11-08 13:27:18 +0100

    authorization feature added

--- src/services/apiAuth.js
+++ src/services/apiAuth.js
@@ -10,3 +10,19 @@ export async function login({ email, password }) {
 
   return data;
 }
+
+export async function getCurrentUser() {
+  const { data: session } = await supabase.auth.getSession();
+
+  if (!session.session) return null;
+
+  // can get user from session, but better to re-download from supabase
+
+  const { data, error } = await supabase.auth.getUser();
+
+  console.log(data);
+
+  if (error) throw new Error(error.message);
+
+  return data?.user;
+}
